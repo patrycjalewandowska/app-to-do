96f9ad8e54f35d4eeb1e98affa3a0343fb7828b6
patrycjalewandowska <24plewandowska@gmail.com>
2024-01-07 09:09:03 +0100

    commented reload

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';  // Zakładam, że używasz React Router
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
